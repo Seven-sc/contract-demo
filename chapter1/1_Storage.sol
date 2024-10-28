781f45fa31b94c67ed6677b6446989a56cefba9c
Seven-sc <xuweijia627@gmail.com>
2024-10-28 23:50:21 +0800

    2

--- chapter1/1_Storage.sol
+++ chapter1/1_Storage.sol
@@ -10,7 +10,7 @@ pragma solidity >=0.8.2 <0.9.0;
 contract Storage {
 
     uint256 number;
-    string message = "hello world1";
+    string message = "hello world";
 
     function setMessage(string memory msg) public {
         message = msg;
