6e2f25baead27d64c9f769134243b6583d942420
Seven-sc <xuweijia627@gmail.com>
2024-11-25 18:01:30 +0800

    '测试多重继承ERC721'

---
+++ chapter1/jicheng/MyErc721.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: GPL-3.0
+
+// pragma solidity >=0.8.2 <0.9.0;
+
+// import "@openzeppelin/contracts/token/ERC721/extensions/ERC721"
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract MyErc721  {
+
+}
