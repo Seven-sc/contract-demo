d51c6472ba4c70435739021021c2bdaea0bd14fb
Seven-sc <xuweijia627@gmail.com>
2024-11-26 11:36:44 +0800

    'MyErc721'

--- chapter1/jicheng/MyErc721.sol
+++ chapter1/jicheng/MyErc721.sol
@@ -1,12 +1,30 @@
 // SPDX-License-Identifier: GPL-3.0
 
-// pragma solidity >=0.8.2 <0.9.0;
+pragma solidity >=0.8.2 <0.9.0;
 
-// import "@openzeppelin/contracts/token/ERC721/extensions/ERC721"
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
+// import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract MyErc721  {
+contract MyErc721 is ERC721URIStorage, ERC721Enumerable {
 
+    constructor() ERC721("mynft", "myNFT") {
+
+    }
+
+    function _increaseBalance(address account, uint128 amount) internal override(ERC721, ERC721Enumerable) {
+        super._increaseBalance(account, amount);
+    }
+
+    function _update(address to, uint256 tokenId, address auth) internal override(ERC721, ERC721Enumerable) returns (address) {
+        return super._update(to, tokenId, auth);
+    }
+
+    function supportsInterface(bytes4 interfaceId) public view override(ERC721Enumerable, ERC721URIStorage) returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }
+
+    function tokenURI(uint256 tokenId) public view override(ERC721, ERC721URIStorage) returns (string memory) {
+        return super.tokenURI(tokenId);
+    }
 }
